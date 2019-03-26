85de15e3f347e43d43662c51c1e70f97d76459dd
Simon Fishel <sfishel@splunk.com>
2019-03-26 09:41:20 -0700

    add some meta info to echo function

--- echo/echo.js
+++ echo/echo.js
@@ -1,10 +1,15 @@
 const { of } = require("rxjs");
 const { delay } = require("rxjs/operators");
 
+const meta = {
+  environment: process.env.RXJS_FIDDLE_ENV,
+  version: process.env.RXJS_FIDDLE_VERSION
+};
+
 module.exports = {
   main: function(event, context) {
     console.log(event);
-    return of({ response: event.data })
+    return of({ response: event.data, meta })
       .pipe(delay(1000))
       .toPromise();
   }
