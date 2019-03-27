0c5552e83385cdfb612276870bc83438eb644a97
Simon Fishel <sfishel@splunk.com>
2019-03-26 20:26:05 -0700

    fix incorrect env variable name

--- deploy-all.sh
+++ deploy-all.sh
@@ -24,11 +24,11 @@ deployFunction() {
     if [ -z "$(kubeless "function" ls $name --namespace $ns 2> /dev/null)" ]
     then
         echo "creating function $name..."
-        kubeless function deploy "$name" --runtime nodejs8 --namespace "$ns" --handler "$name.main" --from-file "$name/$name.js" --dependencies "$name/package.json" --env "RXJS_FIDDLE_ENV=$ns,RXJS_FIDDLE_VERSION=$version"
+        kubeless function deploy "$name" --runtime nodejs8 --namespace "$ns" --handler "$name.main" --from-file "$name/$name.js" --dependencies "$name/package.json" --env "RXJS_FIDDLE_NS=$ns,RXJS_FIDDLE_VERSION=$version"
         kubeless trigger http create "$name" --function-name "$name" --hostname "$hostname" --path "$name" --namespace "$ns" --cors-enable
     else
         echo "updating function $name..."
-        kubeless function update "$name" --runtime nodejs8 --namespace "$ns" --handler "$name.main" --from-file "$name/$name.js" --dependencies "$name/package.json" --env "RXJS_FIDDLE_ENV=$ns,RXJS_FIDDLE_VERSION=$version"
+        kubeless function update "$name" --runtime nodejs8 --namespace "$ns" --handler "$name.main" --from-file "$name/$name.js" --dependencies "$name/package.json" --env "RXJS_FIDDLE_NS=$ns,RXJS_FIDDLE_VERSION=$version"
         kubeless trigger http update "$name" --function-name "$name" --hostname "$hostname" --path "$name" --namespace "$ns"
     fi
 }
