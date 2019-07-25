5d166df1eb4d6df8ec26166e1f5fbd49c6edcada
Simon Fishel <sfishel@splunk.com>
2019-07-24 20:55:13 -0700

    add cors support to function endpoints

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const echo = require("./functions/echo");
 
 const functions = { echo };
@@ -13,7 +14,7 @@ const port = 8080;
 app.use(express.json());
 
 Object.keys(functions).forEach(name => {
-  app.all(`/${name}`, (req, res) => {
+  app.all(`/${name}`, cors(), (req, res) => {
     const ret = functions[name]({ data: req.body });
     if (ret instanceof Promise) {
       ret.then(response => res.json({ response, meta }));
