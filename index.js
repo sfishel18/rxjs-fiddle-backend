5622d644a8a44376425605f61346870613974992
Simon Fishel <simonf@gmail.com>
2020-07-02 21:08:44 -0700

    fix url for tracing

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('signalfx-tracing').init({
   service: 'rxjs-fiddle-backend',
-  url: `'http://${process.env.SIGNALFX_AGENT_HOST}:9080/v2/trace`,
+  url: `'http://${process.env.SIGNALFX_AGENT_HOST}:9080/v1/trace`,
 });
 
 const express = require("express");
